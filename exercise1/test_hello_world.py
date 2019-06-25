4895521389b07e23186bb8f4002a2995b3450d6e
Robin van de Ven <robin.vande.ven@sogeti.com>
2019-06-25 20:50:35 +0200

    First excercise, get element by tagname

--- exercise1/test_hello_world.py
+++ exercise1/test_hello_world.py
@@ -2,4 +2,7 @@ from selenium import webdriver
 
 def test_hello_world():
     browser = webdriver.Chrome()
+    browser.get("http://www.vinwij.nl/selenium/factuur.html")
+    header = browser.find_element_by_tag_name("h3")
+    assert header.text == "Factuur 143256"
     browser.quit()
